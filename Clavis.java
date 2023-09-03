dcc8d75f75901f1ad45276979853c527651c7f06
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-09-02 19:23:50 -0700

    Clavis.java modified

--- Clavis.java
+++ Clavis.java
@@ -1,8 +1,9 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Clavis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         int option;
         Scanner input = new Scanner(System.in);
         String fileName = "./Clavis.txt";
@@ -18,17 +19,46 @@ public class Clavis {
         input.close();
     }
 
-    public static void displayData(String fileName) {
+    public static void sort(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         if (!file.canRead()) {
             System.out.println("ERROR: Permision to read from file denied");
             System.exit(1);
         }
 
-        System.out.println();
-        System.out.println(" ID |               Platform                 |     Description    ");
-        System.out.println(
-                "--------------------------------------------------------------------");
+        Scanner input = new Scanner(file);
+
+    }
+
+    public static void displayData(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        if (!file.canRead()) {
+            System.out.println("ERROR: Permision to read from file denied");
+            System.exit(1);
+        }
+
+        Scanner input = new Scanner(file);
+
+        while (input.hasNext()) {
+            String id = input.next();
+            String platform = input.next();
+            String description = input.next();
+            String email = input.next();
+            String name = input.next();
+            String password = input.next();
+            String other = input.next();
+            System.out.println();
+            System.out.printf("Platform    | %s\n", platform);
+            System.out.printf("Description | %s\n", description.equals("*") ? "" : description);
+            System.out.printf("Email       | %s\n", email.equals("*") ? "" : email);
+            System.out.printf("Name        | %s\n", name.equals("*") ? "" : name);
+            System.out.printf("Password    | %s\n", password);
+            System.out.printf("Forget key  | %s\n", other.equals("*") ? "" : other);
+            System.out.printf("ID          | %s\n", id);
+            System.out.println();
+        }
+
+        input.close();
     }
 
     public static int getOption(Scanner input) {
